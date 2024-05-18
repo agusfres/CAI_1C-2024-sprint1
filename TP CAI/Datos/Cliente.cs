cafd8b6557c5cc88a50c9746cb3db942f3584fa0
agusfres <agustina.fresco@gmail.com>
2024-05-18 10:43:54 -0300

    Empezando cliente

---
+++ TP CAI/Datos/Cliente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class Cliente 
+    {
+        Guid idCliente;
+        string nombre;
+        string apellido;
+        string direccion;
+        string telefono;
+        string email;
+        DateTime fechaAlta;
+        DateTime fechaNacimiento;
+        DateTime? fechaBaja;
+        Guid idUsuario;
+        int host;
+        int dni;
+
+
+        public Cliente(Guid idCliente, string nombre, string apellido, string direccion, string telefono, string email, DateTime fechaAlta, DateTime fechaNacimiento, DateTime? fechaBaja, Guid idUsuario, int host, int dni)
+        {
+            this.idCliente = idCliente;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.email = email;
+            this.fechaAlta = fechaAlta;
+            this.fechaNacimiento = fechaNacimiento;
+            this.fechaBaja = fechaBaja;
+            this.idUsuario = idUsuario;
+            this.dni = dni;
+            this.host = host;
+
+        }
+
+
+        public Guid IdCliente { get => idCliente; set => idCliente = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime FechaAlta { get => fechaAlta; set => fechaAlta = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public DateTime? FechaBaja { get => fechaBaja; set => fechaBaja = value; }
+        public Guid IdUsuario { get => idUsuario; set => idUsuario = value; }
+        public int Dni { get => dni; set => dni = value; }
+        public int Host { get => host; set => host = value; }
+    }
+}
